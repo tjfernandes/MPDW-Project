5d00f6a5a3a7b25ce0c1db8f818389eb7c1fb55d
tjFernandes <tj.fernandes@campus.fct.unl.pt>
2024-04-07 19:58:59 +0100

    mapping test update

---
+++ recipes/.ipynb_checkpoints/config-checkpoint.py
@@ -0,0 +1,9 @@
+import os
+
+CONFIG = {
+    "host": "api.novasearch.org",
+    "port": 443,
+    "index_name": "user219",
+    "user": 'user219', # mudar depois para variáveis de ambiente
+    "password": '=AML*G^Y7S:n,dX', # mudar depois para variáveis de ambiente
+}
