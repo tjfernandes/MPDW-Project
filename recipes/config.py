4b152398a8f167e91d6fd8dbaabd7ba050474e0b
tjFernandes <tj.fernandes@campus.fct.unl.pt>
2024-04-08 12:35:58 +0100

    added for loop to add the recipes to the index

--- recipes/config.py
+++ recipes/config.py
@@ -3,7 +3,7 @@ import os
 CONFIG = {
     "host": "api.novasearch.org",
     "port": 443,
-    "index_name": "user219",
+    "index_name": "recipes",
     "user": 'user219', # mudar depois para variáveis de ambiente
     "password": '=AML*G^Y7S:n,dX', # mudar depois para variáveis de ambiente
 }
