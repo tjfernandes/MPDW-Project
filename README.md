f1a6e24cc1bb02790421a40f98e7d8549c3b0c24
tjFernandes <tj.fernandes@campus.fct.unl.pt>
2024-04-08 22:41:36 +0100

    added boolean search with filter

--- README.md
+++
@@ -1,1 +0,0 @@
-# MPDW-Project
--- recipes/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-CONFIG = {
-    "host": "api.novasearch.org",
-    "port": 443,
-    "index_name": "recipes",
-    "user": 'user219', # mudar depois para variáveis de ambiente
-    "password": '=AML*G^Y7S:n,dX', # mudar depois para variáveis de ambiente
-}
