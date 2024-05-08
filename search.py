a62edab6b004714393c4bdcf432f6e06260c00c3
tjFernandes <tj.fernandes@campus.fct.unl.pt>
2024-05-09 00:24:51 +0100

    improved search by mixing with boolean search

--- search.py
+++ search.py
@@ -5,14 +5,25 @@ def text_query(client, index_name, query):
     query_emb = embeddings.encode(query)
 
     query_denc = {
-    'size': 3,
-    '_source': ['title', 'description', 'ingredients'],
-    "query": {
-            "knn": {
-            "title_embedding": {
-                "vector": query_emb[0].numpy(),
-                "k": 2
-            }
+        'size': 3,
+        '_source': ['title', 'description'],
+        "query": {
+            "bool": {
+                "should": [
+                    {
+                        "knn": {
+                            "title_embedding": {
+                                "vector": query_emb[0].numpy(),
+                                "k": 2
+                            }
+                        }
+                    },
+                    {
+                        "match": {
+                            "title": query
+                        }
+                    }
+                ]
             }
         }
     }
